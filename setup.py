1b6f4fe28518c5148f239c7c4f340ec88d0cd625
Shahriar Rahman <shahriar.rahman10@northsouth.edu>
2023-05-08 10:13:32 +0600

    main files commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an exploratory data analysis on a smart home system data set.',
+    author='Shahriar',
+    license='',
+)
